694bc0a55b215a29243aeb5c5a40f272d752bf40
Gowtham200519 <gowthams192005@gmail.com>
2025-06-15 16:43:00 +0530

    its a task
    Hi, i am gowtham This repository contains my internship task completed using Python. It demonstrates my understanding of core Python concepts and practical coding skills.

---
+++ python program task 01.py
@@ -0,0 +1,9 @@
+stored_username = "football"
+stored_password = "2005"
+username =  input("enter your username:")
+password = input("enter your password:")
+if username == stored_username and password == stored_password :
+              print("login successfully, welcome" ,username)
+else:
+    print("invalid username and password")
+            
